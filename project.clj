29036b863c4d3abe01e444b04780bfb506c3942a
Reid 'arrdem' McKenzie <reid@factual.com>
2015-08-27 00:38:01 -0500

    Upgrade deps

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/Raynes/least"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.2"]
-                 [cheshire "5.1.1"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
+                 [cheshire "5.5.0"]])
